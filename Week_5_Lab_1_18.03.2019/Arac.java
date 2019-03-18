f04da3be900ea9302c76a138a2ccba22c1afa283
paucomputereng <44471672+paucomputereng@users.noreply.github.com>
2019-03-18 19:16:35 +0300

    Create Arac.java

---
+++ Week_5_Lab_1_18.03.2019/Arac.java
@@ -0,0 +1,11 @@
+public class Arac {
+
+    public String sahipIsmi;
+    public int ilNo;
+
+    public Arac(String isim, int ilNo){
+        this.sahipIsmi = isim;
+        this.ilNo = ilNo;
+    }
+
+}
