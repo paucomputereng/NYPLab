3f718b200e7939474d223958066487b9212bdcb4
paucomputereng <44471672+paucomputereng@users.noreply.github.com>
2019-05-13 16:54:17 +0300

    Add files via upload

---
+++ Week_14_Lab_1_13.05.2019/GUIDBDers/src/guidbders/Item.java
@@ -0,0 +1,38 @@
+package guidbders;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acar
+ */
+ class Item
+    {
+        private int id;
+        private String description;
+
+        public Item(int id, String description)
+        {
+            this.id = id;
+            this.description = description;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public String toString()
+        {
+            return description;
+        }
+    }
